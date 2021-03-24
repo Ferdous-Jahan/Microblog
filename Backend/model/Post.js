bdbeec4326fcb370fe0b7f5cf7c3d4ff25dd60ef
Ferdous Jahan <ferdousjahan2@gmail.com>
2021-03-24 14:25:15 +0600

    backend complete wit REST API

---
+++ Backend/model/Post.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    comments: [
+      {
+        userId: String,
+        name: String,
+        comment: String,
+      },
+      {
+        timestamps: true,
+      },
+    ],
+    likes: [String],
+    dislikes: [String],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", postSchema);
